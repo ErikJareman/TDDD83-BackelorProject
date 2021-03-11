e4949ce70e85966902ef6a1ada570ef56a98b3b9
albinjal <albin.jaldevik@gmail.com>
2021-03-12 00:21:53 +0100

    router foundation

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,27 +1,8 @@
 import $ from 'jquery';
-
-// Interfaces är lite annorlunda från Java
-// Här används de ofta för att beskriva ett object {}
-interface testType {
-    test: string;
-    age: number;
-}
-
-// Arrow function med typer för parametrar och output
-const createHTML = (input: testType): string => {
-    return `
-    <p>${input.test}</p>
-    <p>Jag är ${input.age} år gammal.</p>
-    `;
-};
+import { initiateRouter } from './core/router';
 
 const main = () => {
-    const data: testType = {
-        test: 'Hello World!',
-        age: 12,
-    };
-
-    $('#app').html(createHTML(data));
+    initiateRouter();
 };
 
 $(main);
