1cb3f9f105226eb9aa20d98b2321692c8f0cd631
erija971 <erija971@student.liu.se>
2021-03-11 15:52:09 +0100

    Mycket html samt en del scss för rumhantering

--- client/src/index.ts
+++ client/src/index.ts
@@ -21,7 +21,41 @@ const main = () => {
         age: 12,
     };
 
-    $('#app').html(createHTML(data));
+    //$('#app').html(createHTML(data));
 };
+
+
+
+
+
+function ticketTemplate(ticket) {
+    return `<div class="card car-card">
+    <table>
+        <tr>
+            <td>Märke:</td><td>${car.make}</td>
+        </tr>
+        <tr>
+            <td>Modell:</td><td>${car.model}</td>
+        </tr>
+        <tr>
+            <td>Kund:</td><td>${car.customer ? car.customer.name: 'Kund saknas'}</td>
+        </tr>
+    </table>
+    <br>
+    <span>
+        <button data-id=${car.id} class="btn btn-primary edit-car" data-toggle="modal" data-target="#editModal">Redigera</button>
+        <button data-id=${car.id} class="btn btn-danger delete-car">Ta bort</button>
+    </span>
+</div>`;
+}
+function loadTickets() {
+    const tickets = server.getTickets();
+    const ticketListElement = $("#ticket-list")
+    ticketListElement.empty();
+    for (ticket of tickets) {
+        ticketListElement.append(ticketTemplate(ticket));
+    }
+    //$(".delete-car").click(clickDelete); add listeners like this...
+}
 
 $(main);
