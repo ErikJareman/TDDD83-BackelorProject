45ce3663188fc8bc12c0182f599143886fc07b8f
Sara Abrahamsson <sarab579@student.liu.se>
2021-03-11 09:24:37 +0100

    Back-end och front-end fungerar separat, saknas Ajax

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,4 +1,5 @@
 import $ from 'jquery';
+import env from './shared/env';
 
 // Interfaces är lite annorlunda från Java
 // Här används de ofta för att beskriva ett object {}
@@ -14,6 +15,46 @@ const createHTML = (input: testType): string => {
     <p>Jag är ${input.age} år gammal.</p>
     `;
 };
+
+function createUser(){
+    var name = document.getElementById("register-name").value;
+    var email = document.getElementById("register-email").value;
+    var password = document.getElementById("register-password").value;
+    $.ajax({
+        url: env.backendURL + '/sign-up',
+        type: 'POST',
+        dataType: "json",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "name": name,
+            "email": email,
+            "password": password
+        }),
+        success: function(){
+            $(".container").html($("#view-home").html());
+        }
+    })
+}
+
+function logIn(){
+    var email = document.getElementById("register-email").value;
+    var password = document.getElementById("register-password").value;
+    $.ajax({
+        url: host + '/login',
+        type: 'POST',
+        dataType: "json",
+        contenttype: 'application/json',
+        data: JSON.stringify({
+            "email": email,
+            "password": password
+        }),
+        success: function(result){
+            sessionStorage.setItem('auth', JSON.stringify(result));
+            window.location.reload();
+            console.log(result);
+        }
+    })
+}
 
 const main = () => {
     const data: testType = {
