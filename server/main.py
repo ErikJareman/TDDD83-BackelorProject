7592af5013988c250a70f90bbcdf86433ec7f50c
erija971 <erija971@student.liu.se>
2021-03-12 10:46:54 +0100

    Skapade databasen

--- server/main.py
+++ server/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
-@app.route('/<string:route>', methods=['GET'])
-def client(route):
+@app.route('/', methods=['GET'])
+def client():
     return app.send_static_file("index.html")
 
 if __name__ == "__main__":
