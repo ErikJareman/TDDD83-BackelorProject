cdf0621776d7d068ffd2f7351429f7acbf74c3ae
albinjal <albin.jaldevik@gmail.com>
2021-04-06 13:40:22 +0200

    hotfix

--- client/src/core/endpoints.ts
+++ client/src/core/endpoints.ts
@@ -2,4 +2,7 @@ export enum EndPoints {
     Rooms = 'rooms',
     Tickets = 'tickets',
     LeaveRoom = 'leave-room',
+    PromoteMember = 'promote-member',
+    DemoteMember = 'demote-member',
+    DeleteTicket = 'delete-ticket',
 }
