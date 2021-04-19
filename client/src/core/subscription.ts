92ad6e297165e6c5655a2d122c61e68d2d40ff89
albinjal <albin.jaldevik@gmail.com>
2021-04-19 13:17:04 +0200

    refactor stipe intergation

--- client/src/core/subscription.ts
+++ client/src/core/subscription.ts
@@ -1,4 +1,5 @@
-import env from '../shared/env';
+import { EndPoints } from './endpoints';
+import { standardDelete, standardGet, standardPost } from './server.service';
 
 export interface JWTData {
     fresh: boolean;
@@ -10,36 +11,15 @@ export interface JWTData {
 
 export function addAdmin() {
     const email = $<HTMLInputElement>('#inputemail').val();
-    try {
-        const result = fetch('env.backendURL/school_admin', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + JSON.parse(sessionStorage.getItem('auth')).token,
-            },
-            body: JSON.stringify({
-                admin_email: email,
-            }),
-        });
-        writeAdmins();
-    } catch (e) {
-        // TODO
-    }
+
+    standardPost(EndPoints.SchoolAdmin, { admin_email: email });
+    writeAdmins();
 }
 
 export function deleteAdmin() {
     const email = $<HTMLInputElement>('#Admin-delete').val();
     try {
-        const result = fetch('env.backendURL/school_admin', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + JSON.parse(sessionStorage.getItem('auth')).token,
-            },
-            body: JSON.stringify({
-                admin_email: email,
-            }),
-        });
+        standardDelete(EndPoints.SchoolAdmin, { admin_email: email });
         writeAdmins();
     } catch (e) {
         alert('This is not one of your admins. Try again.');
@@ -47,22 +27,11 @@ export function deleteAdmin() {
 }
 
 export async function modalDelete() {
-    try {
-        const result = await fetch('env.backendURL/school_admin', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + JSON.parse(sessionStorage.getItem('auth')).token,
-            },
-        });
-        const admins = await result.json();
-        $('#Admin-delete').append(`<option selected>Current admins</option>`);
-        admins.forEach((School_Admin) => {
-            $('#Admin-delete').append(`<option>${School_Admin.admin_email}</option>`);
-        });
-    } catch (e) {
-        //TODO
-    }
+    const admins = await standardGet(EndPoints.SchoolAdmin);
+    $('#Admin-delete').append(`<option selected>Current admins</option>`);
+    admins.forEach((School_Admin) => {
+        $('#Admin-delete').append(`<option>${School_Admin.admin_email}</option>`);
+    });
 }
 
 export async function writeAdmins() {
@@ -72,19 +41,12 @@ export async function writeAdmins() {
     $('#admin-admin').empty();
     if (max_admin > 0) {
         try {
-            const result = await fetch('env.backendURL/school_admin', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: 'Bearer ' + JSON.parse(sessionStorage.getItem('auth')).token,
-                },
-            });
+            const admins = await standardGet(EndPoints.SchoolAdmin);
             //    console.log(max_admin);
-            const admins = await result.json();
             let number_of = 0;
             // console.log(admins);
             admins.forEach((School_Admin) => {
-                number_of++;
+                ++number_of;
                 $('#admin-admin').append(` 
                 <tr>
                   <th scope="row">${number_of}</th>
@@ -101,14 +63,8 @@ export async function writeAdmins() {
 
 export async function checkSubscription() {
     try {
-        const result = await fetch('env.backendURL/update-school', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer ' + JSON.parse(sessionStorage.getItem('auth')).token,
-            },
-        });
-        const subscription = await result.json();
+        const subscription = await standardPost(EndPoints.UpdateSchool);
+
         return subscription.School;
     } catch (e) {
         // TODO
